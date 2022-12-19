5ed7115a97f9d8523c012c388a5c25641891171e
fawazming <91906160+fawazming@users.noreply.github.com>
2022-12-19 12:40:35 +0100

    Disable online payment

--- ci/app/Views/buypin.php
+++ ci/app/Views/buypin.php
@@ -1,8 +1,9 @@
 
         <h5><a href="<?=base_url('/')?>">Home</a> > Buy pin for registeration</h5>
         <div class="row">
-            <a href="<?=base_url('payonline')?>" class="col border">
-                <h2>Pay Online </h2>
+            <!--<a href="<?=base_url('payonline')?>" class="col border">-->
+             <a href="#!" class="col border">
+                <del><h2>Pay Online </h2></del>
                 <small>Instant Pin Vending for registeration</small>
             </a>
             <a href="<?=base_url('vendors')?>" class="col border">
